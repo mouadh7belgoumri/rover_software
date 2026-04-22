7f018b9c9d2709096b6783dbb1d93a6dd456e800
mouadh7belgoumri <m_belgoumri@estin.dz>
2026-04-22 14:23:10 +0100

    finnished code for one sensor, still not tested

--- ultrasonic_sensors/src/main.cpp
+++ ultrasonic_sensors/src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
+#include <Servo.h>
+#define BAUD_RATE 9600
+#define ECHO_PIN 10
+#define TRIG_PIN 11
+#define SERVO_PIN 9
 
-// put function declarations here:
-int myFunction(int, int);
+Servo mtr;
+uint64_t pulseDuration;
+uint64_t distance;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  Serial.begin(BAUD_RATE);
+  mtr.attach(SERVO_PIN);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  for (int pos; pos <= 180; pos++)
+  {
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10),
+    pulseDuration = pulseIn(ECHO_PIN, HIGH);
+    distance = (pulseDuration * .0343) / 2;
+    Serial.print("Distance: ");
+    Serial.println(distance);
+    mtr.write(pos);
+    delay(10);
+    Serial.println("position : ");
+    Serial.print(pos);
+  }
 }
